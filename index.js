2a4b982bec5f4826bb7290cd0bf651cf89137408
Ayush Tiwari <ayush.tiwari@team.cliffex.com>
2023-03-01 14:24:12 +0530

    set up routers and controlllers

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const port = 8000;
 const app = express();
 
+//use Express Routers
+app.use('/',require('./routes'));
+
+//Set up the View engine
+app.set('view engine','ejs')
+app.set('views','./view')
 
 app.listen(port,function(err){
     if(err){console.log(`Error: ${err}`);}
