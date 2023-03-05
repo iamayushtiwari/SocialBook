a7e7387cabccfbb317a9cd5b5935c9cf1eef9aae
Ayush Tiwari <ayush.tiwari@team.cliffex.com>
2023-03-05 19:45:36 +0530

    set static files acess for layouts and pages

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const port = 8000;
 const app = express();
+const expressLayout = require('express-ejs-layouts')
+app.use(expressLayout)
+
+app.use(express.static('./assets'))
+
+// add dynamic style and script true 
+app.set('layout extractStyles',true)
+app.set('layout extractScripts',true)
 
 //use Express Routers
 app.use('/',require('./routes'));
