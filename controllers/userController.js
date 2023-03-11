2f5e82d54fa5f55465e6f74fe77aecec91df1001
Ayush Tiwari <ayushtiwari653@gmail.com>
2023-03-12 00:32:54 +0530

    manual local authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,20 @@
 const User = require('../models/user_Schema')
 
 module.exports.profile = function (req, res) {
-    // return res.end('<h1>User Profile </h1>');
-    return res.render('userProfile', {
-        title: 'User Profile'
-    })
+    if (req.cookies.user_id) {
+        User.findById(req.cookies.user_id).then(user => {
+            if (user) {
+                return res.render('userProfile', {
+                    title: 'User Profile',
+                    user:user
+                })
+            } else {
+                return res.redirect('/users/signin')
+            }
+        })
+    } else {
+        return res.redirect('/users/signin')
+    }
 }
 module.exports.signin = function (req, res) {
     return res.render('userSignin', {
@@ -17,6 +27,11 @@ module.exports.signup = function (req, res) {
         title: 'User Signup'
     })
 }
+module.exports.signout = function(req,res){ 
+    res.clearCookie('user_id')
+    
+    res.redirect('/users/signin')
+}
 //sigup and create a new user
 module.exports.create = function (req, res) {
     if (req.body.password != req.body.confirmPassword) {
@@ -26,7 +41,7 @@ module.exports.create = function (req, res) {
         if (!user) {
             User.create(req.body).then(user => {
                 return res.redirect('/users/signin')
-            }),function (err) {
+            }), function (err) {
                 console.log(err)
             }
         }
@@ -39,5 +54,22 @@ module.exports.create = function (req, res) {
 }
 //sigin and create a session for user
 module.exports.createSession = function (req, res) {
-    //todo later
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            // password validation 
+            if (user.password != req.body.password) {
+                console.log("password does not matched")
+                return res.redirect('back')
+            }
+            else {
+                // create session
+                res.cookie('user_id', user.id)
+                return res.redirect('/users/profile')
+            }
+        }
+        else {
+            console.log("user not found")
+            return res.redirect('back');
+        }
+    })
 }
