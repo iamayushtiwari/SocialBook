ec86655992ed85dbc2078a617d5b88b57557ab0a
Ayush Tiwari <ayush.tiwari@team.cliffex.com>
2023-03-12 00:32:54 +0530

    manual local authentication

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ router.get('/profile',UserController.profile)
 router.get('/post', postController.post)
 router.get('/signin',UserController.signin)
 router.get('/signup',UserController.signup)
+router.get('/logout',UserController.signout)
 
 router.post('/create',UserController.create)
 router.post('/create-session',UserController.createSession)
