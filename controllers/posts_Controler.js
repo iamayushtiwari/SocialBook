df8942b8fe9349d3af0c06db4e5847b87d23fc41
Ayush Tiwari <ayushtiwari653@gmail.com>
2023-05-19 12:26:11 +0530

    post create done

--- controllers/posts_Controler.js
+++ controllers/posts_Controler.js
@@ -4,7 +4,7 @@ module.exports.post = function(req, res){
 }
 
 module.exports.createPost = function(req, res){
-    // return res.end('<h1>post is rendaring</h1>')
+    // return res.end('<h1>create post is rendaring</h1>')
     Post.create({context:req.body.context,user:req._id}).then(post => {
         return res.redirect('back')
     }),function(err){
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController')
-const postController = require('../controllers/postControler')
 const passport = require('passport')
 
 router.get('/profile',passport.checkAuthentication,UserController.profile)
-router.get('/post', postController.post)
 router.get('/signin',UserController.signin)
 router.get('/signup',UserController.signup)
 router.get('/signout',UserController.destroySession)
 
 
 router.post('/create',UserController.create)
-router.post('/create-post', postController.createPost)
 router.post('/create-session',passport.authenticate('local', { failureRedirect: '/users/signin'}),
 UserController.createSession)
 
