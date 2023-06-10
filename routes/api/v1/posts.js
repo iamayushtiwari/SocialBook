ebc2b578a773b4c7948eb82b9d8895fcf2e968c8
Ayush Tiwari <ayushtiwari653@gmail.com>
2023-06-10 14:04:08 +0530

    update routers

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const postApi = require('../../../controllers/api/v1/post_API')
 
-router.use('/posts',require('./posts'))
-router.use('/user',require('./users'))
+router.get('/posts',postApi.index)
+router.delete('/:id',postApi.destroy)
 
 module.exports = router
